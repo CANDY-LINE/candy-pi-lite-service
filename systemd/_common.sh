9b5609b899ba8a84ee27daef82a4ef595758c69c
Daisuke Baba <baba.daisuke@gmail.com>
2017-07-01 18:15:09 +0900

    Improve serial port locating

--- systemd/_common.sh
+++ systemd/_common.sh
@@ -18,7 +18,23 @@ function log {
 
 function look_for_serial_port {
   MODEM_SERIAL_PORT=`/usr/bin/env python -c "import candy_board_qws; print(candy_board_qws.SerialPort.resolve_modem_port())"`
-  log "Serial port: ${MODEM_SERIAL_PORT} is selected"
+  if [ "${MODEM_SERIAL_PORT}" == "None" ]; then
+    if [ -e "${UART_PORT}" ]; then
+      log "[INFO] Trying to adjust baudrate"
+      MODEM_BAUDRATE=`/usr/bin/env python -c "import candy_board_qws; print(candy_board_qws.SerialPort.resolve_modem_baudrate('${UART_PORT}'))"`
+      if [ "${MODEM_BAUDRATE}" != "None" ]; then
+        MODEM_SERIAL_PORT=${UART_PORT}
+      else
+        log "[ERROR] Serial port is missing, good-bye"
+        exit 10
+      fi
+    else
+      log "[ERROR] Serial port is missing, bye"
+      exit 10
+    fi
+  else
+    log "Serial port: ${MODEM_SERIAL_PORT} is selected"
+  fi
 }
 
 function init_serialport {
@@ -33,9 +49,11 @@ function init_serialport {
     if [ "${MODEM_BAUDRATE}" != "${BAUDRATE}" ]; then
       candy_command modem "{\"action\":\"init\",\"baudrate\":\"${BAUDRATE}\"}"
       MODEM_BAUDRATE=${MODEM_BAUDRATE:-${BAUDRATE}}
+      log "[INFO] Modem baudrate changed: ${MODEM_BAUDRATE} => ${BAUDRATE}"
+    else
+      log "[INFO] Modem baudrate => ${MODEM_BAUDRATE}"
     fi
   fi
-  log "[INFO] Modem baudrate => ${MODEM_BAUDRATE}"
 }
 
 function candy_command {
