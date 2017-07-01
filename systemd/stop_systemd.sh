c4aff5a541287e234d00e0f1a7107a5d409bffa4
Daisuke Baba <baba.daisuke@gmail.com>
2017-07-01 18:14:20 +0900

    Call GPIO setup functions

--- systemd/stop_systemd.sh
+++ systemd/stop_systemd.sh
@@ -6,6 +6,8 @@ SHUDOWN_STATE_FILE=/opt/candy-line/${PRODUCT_DIR_NAME}/__shutdown
 
 function led_off {
   . /opt/candy-line/${PRODUCT_DIR_NAME}/_pin_settings.sh > /dev/null 2>&1
+  setup_ports
+  setup_pin_directions
   echo 0 > ${LED2_PIN}/value
 }
 
