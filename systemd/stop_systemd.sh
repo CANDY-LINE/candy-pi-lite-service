72fbd369d54242ae24e2c7fe75fe455f7316aef1
Daisuke Baba <baba.daisuke@gmail.com>
2017-07-15 13:59:04 +0900

    Refactor shutdown script

--- systemd/stop_systemd.sh
+++ systemd/stop_systemd.sh
@@ -4,10 +4,18 @@ PRODUCT="CANDY Pi Lite Board"
 PRODUCT_DIR_NAME="candy-pi-lite"
 SHUDOWN_STATE_FILE=/opt/candy-line/${PRODUCT_DIR_NAME}/__shutdown
 
+function init {
+  . /opt/candy-line/${PRODUCT_DIR_NAME}/_common.sh > /dev/null 2>&1
+  if [ -e "${UART_PORT}" ] || [ -e "${QWS_UC20_PORT}" ] || [ -e "${QWS_EC21_PORT}" ]; then
+    . /opt/candy-line/${PRODUCT_DIR_NAME}/_pin_settings.sh > /dev/null 2>&1
+    export LED2
+  else
+    log "[ERROR] Modem is missing"
+    exit 11
+  fi
+}
+
 function led_off {
-  . /opt/candy-line/${PRODUCT_DIR_NAME}/_pin_settings.sh > /dev/null 2>&1
-  setup_ports
-  setup_pin_directions
   echo 0 > ${LED2_PIN}/value
 }
 
@@ -15,8 +23,8 @@ function led_off {
 logger -t ${PRODUCT_DIR_NAME} "Inactivating ${PRODUCT}..."
 touch ${SHUDOWN_STATE_FILE}
 
+init
 poff
-/opt/candy-line/${PRODUCT_DIR_NAME}/modem_off.sh > /dev/null 2>&1
 led_off
 systemctl restart dhcpcd
 if [ "${NTP_DISABLED}" == "1" ]; then
