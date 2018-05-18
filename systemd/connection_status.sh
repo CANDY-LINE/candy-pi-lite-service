2662ca7a6c3c884deda12fb4ad4417237c5b14f3
Daisuke Baba <baba.daisuke@gmail.com>
2018-05-18 14:52:54 +0900

    Add a new script to show the current connection status

---
+++ systemd/connection_status.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2018 CANDY LINE INC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT="CANDY Pi Lite Board"
+PRODUCT_DIR_NAME="candy-pi-lite"
+DEBUG="1"
+
+function init {
+  . /opt/candy-line/${PRODUCT_DIR_NAME}/_common.sh > /dev/null 2>&1
+}
+
+function test_if_connected {
+  if [ -f "${PPPD_RUNNING_FILE}" ]; then
+    log "[ONLINE] The connection is already established"
+    exit 0
+  else
+    log "[OFFLINE] The connection is not established"
+    exit 1
+  fi
+}
+
+init
+test_if_connected
