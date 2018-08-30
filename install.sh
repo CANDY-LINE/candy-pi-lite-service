887db515e6a83f3786a12d08d6b174912da6012f
Daisuke Baba <baba.daisuke@gmail.com>
2018-08-30 12:21:39 +0900

    Bump version

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ VENDOR_HOME=/opt/candy-line
 
 SERVICE_NAME=candy-pi-lite
 GITHUB_ID=CANDY-LINE/candy-pi-lite-service
-VERSION=5.2.0
+VERSION=6.0.0
 # Channel B
 UART_PORT="/dev/ttySC1"
 MODEM_BAUDRATE=${MODEM_BAUDRATE:-460800}
